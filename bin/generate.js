c3e2199ae7b4fbf9bdabb0a2ee6ee28d331c4239
Ruben Taelman <ruben.taelman@ugent.be>
2016-08-29 14:55:06 +0200

    Create routes instead of single trips

--- bin/generate.js
+++ bin/generate.js
@@ -3,10 +3,16 @@
 const fs = require('fs');
 const csvparse = require('csv-parse');
 const transform = require('stream-transform');
+const _ = require('lodash');
 
 const Point = require('../lib/Point.js');
 const Region = require('../lib/Region.js');
 const TripsVisualizer = require('../lib/TripsVisualizer.js');
+
+function random() {
+    var x = Math.sin(param_seed++) * 10000;
+    return x - Math.floor(x);
+}
 
 var points = [];
 var region = new Region();
@@ -19,16 +25,13 @@ var maxvalue = 0;
 
 var param_seed = 1; // Random seed
 var param_min_station_size = 0.01; // The minimum population density for a station to form
-var param_trips = 100; // The number of trips to generate
+var param_routes = 100; // The number of routes to generate
+var trips_per_route_average = 10; // The average number of trips per route
+var trips_per_route_variation = 2; // The variation in number of trips per route;
 var param_start_stop_choice_power = 4; // Higher values means higher chance on larger stations when selecting starting stations
 var param_target_stop_in_radius_choice_power = 3; // Higher values means higher chance on larger stations when selecting target stations in a radius
 var param_max_trip_distance_factor = 0.5; // The maximum distance of a trip divided by the maximum region diameter
 var param_max_size_difference_factor = 0.5; // The maximum relative distance end stations can have
-
-function random() {
-    var x = Math.sin(param_seed++) * 10000;
-    return x - Math.floor(x);
-}
 
 prepareData();
 function prepareData() {
@@ -74,30 +77,61 @@ function getRandomPointWeightedBySize() {
 }
 
 function generateStopsAndTrips() {
-    // Pick X times a random station
+    // Loop X times
+    // Pick a times a random start station
+    // Loop requiredTrips times (to create a route containing trips
     // Based, on the size, define a radius
     // Within that radius, find X stations
     // Pick random station in that list, weighted by difference in size (exclude stations with a smaller size)
     // Create a trip between those stations
-    for (var i = 0; i < param_trips; i++) {
+    // Set random station as start station
+    // Loop next
+    for (var i = 0; i < param_routes; i++) {
+        var requiredTrips = Math.ceil(((random() - 0.5) * trips_per_route_variation * 2) + trips_per_route_average);
         var point = getRandomPointWeightedBySize();
         while (point.value < param_min_station_size) {
             point = getRandomPointWeightedBySize();
         }
-        var radius = point.value / maxvalue * maxdistance * param_max_trip_distance_factor;
-        var points = region.getPointsInRegion(point.x, point.y, Math.ceil(radius), point.value * param_max_size_difference_factor);
-        points.sort(function(a, b) {
-            return point.value - Math.abs(a.value - b.value);
-        });
-        var targetPoint = getRandomElementWeightedBySize(points, param_target_stop_in_radius_choice_power);
+        var firstPoint = true;
+        var offsetX = 0;
+        var offsetY = 0;
+        for (var j = 0; j < requiredTrips; j++) {
+            var radius = point.value / maxvalue * maxdistance * param_max_trip_distance_factor * (firstPoint ? 1 : 0.5);
+            var points = region.getPointsInRegion(point.x + offsetX, point.y + offsetY, Math.ceil(radius), point.value * param_max_size_difference_factor);
+            points.sort(function (a, b) {
+                return (point.value - Math.abs(a.value - b.value));
+            });
+            var targetPoint = getRandomElementWeightedBySize(points, param_target_stop_in_radius_choice_power);
 
-        if (targetPoint) {
-            region.markStation(point.x, point.y);
-            region.markStation(targetPoint.x, targetPoint.y);
-            trips.push({from: point, to: targetPoint});
+            if (targetPoint) {
+                region.markStation(point.x, point.y);
+                region.markStation(targetPoint.x, targetPoint.y);
+                trips.push({from: point, to: targetPoint});
+
+                // Choose a new point in the direction of the targetPoint
+                // Because in the non-first iteration, the radius will be halved,
+                // which will lead to more straight routes.
+                offsetX = Math.ceil((targetPoint.x - point.x) / 2);
+                offsetY = Math.ceil((targetPoint.y - point.y) / 2);
+                point = targetPoint;
+                firstPoint = false;
+            } else {
+                j = requiredTrips;
+            }
         }
     }
 
+    // Merge similar trips/routes
+    // TODO
+
+
+    // Additional step to make sure ALL stops can be reached
+    // Form clusters (each of them has 1 largest station)
+    // While #clusters > 1
+    //   Pick 1 random clusters
+    //   Find closest cluster based on largest stations
+    //   Determine largest point in each cluster, connect them with a trip, merge clusters
+    // TODO
+
     var visualizer = new TripsVisualizer(region, trips).render("trips.png");
-    // TODO: modify all trips and try to go over stations
 }
