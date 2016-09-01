9a6b03399164c90a6c047e71801e334af5a5b8d1
Ruben Taelman <ruben.taelman@ugent.be>
2016-09-01 16:32:52 +0200

    Calculate stop distances

--- bin/generate.js
+++ bin/generate.js
@@ -2,27 +2,60 @@
 
 const RegionFactory = require('../lib/RegionFactory.js');
 const TripsVisualizer = require('../lib/TripsVisualizer.js');
+const DistanceHelpers = require('../lib/DistanceHelpers.js');
 
 const ParameterizedStopsGenerator = require('../lib/ParameterizedStopsGenerator.js');
 const RandomStopsGenerator = require('../lib/RandomStopsGenerator.js');
 
 // Generate stops based on population distribution
-new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
-    new ParameterizedStopsGenerator(region).generate();
-    new TripsVisualizer(region).render("stops_parameterized.png");
-});
+function getParameterizedStops() {
+    return new Promise((resolve, reject) => {
+        new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
+            new ParameterizedStopsGenerator(region).generate();
+            resolve(region.getStations());
+            //new TripsVisualizer(region).render("stops_parameterized.png");
+        });
+    });
+}
 
 // Generate stops at random
-new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
-    new RandomStopsGenerator(region).generate();
-    new TripsVisualizer(region).render("stops_random.png");
-});
+function getRandomStops() {
+    return new Promise((resolve, reject) => {
+        new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
+            new RandomStopsGenerator(region).generate();
+            resolve(region.getStations());
+            //new TripsVisualizer(region).render("stops_random.png");
+        });
+    });
+}
 
 // Load golden standard of stops
-new RegionFactory('input_data/region_cells.csv', true).createRegion((region) => {
-    new TripsVisualizer(region).render("stops_gs.png");
-});
+function getGoldenStandardtops() {
+    return new Promise((resolve, reject) => {
+        new RegionFactory('input_data/region_cells.csv', true).createRegion((region) => {
+            resolve(region.getStations());
+            //new TripsVisualizer(region).render("stops_gs.png");
+        });
+    });
+}
 
-// TODO:
-// Add function to region to get stop list
-// Compare the two stop lists with the golden standard (calculate distance)
+Promise.all([
+    getParameterizedStops(),
+    getRandomStops(),
+    getGoldenStandardtops()
+])
+    .then(([parameterizedStops, randomStops, goldenStandardStops]) => {
+        console.log("PARAM: " + parameterizedStops.length); // TODO
+        console.log("RAND: " + randomStops.length); // TODO
+        console.log("GS: " + goldenStandardStops.length); // TODO
+
+        // Compare the two stop lists with the golden standard (calculate distance)
+        var distance_param = DistanceHelpers.points(parameterizedStops, goldenStandardStops, DistanceHelpers.point2D);
+        var distance_rand = DistanceHelpers.points(randomStops, goldenStandardStops, DistanceHelpers.point2D);
+        console.log("PARAM distance: " + distance_param); // TODO
+        console.log("RAND distance: " + distance_rand); // TODO
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
