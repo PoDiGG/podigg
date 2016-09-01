9eb9e6d2988ac803af6996a8be7012d3bd170cf5
Ruben Taelman <ruben.taelman@ugent.be>
2016-09-01 16:08:21 +0200

    Add random stops generator

--- bin/generate.js
+++ bin/generate.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const RegionFactory = require('../lib/RegionFactory.js');
-const ParameterizedStopsGenerator = require('../lib/ParameterizedStopsGenerator.js');
 const TripsVisualizer = require('../lib/TripsVisualizer.js');
 
-new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
-    var generator = new ParameterizedStopsGenerator(region);
-    generator.generate();
+const ParameterizedStopsGenerator = require('../lib/ParameterizedStopsGenerator.js');
+const RandomStopsGenerator = require('../lib/RandomStopsGenerator.js');
 
-    new TripsVisualizer(region).render("edges.png");
+new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
+    new ParameterizedStopsGenerator(region).generate();
+    new TripsVisualizer(region).render("stops_parameterized.png");
+});
+
+new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
+    new RandomStopsGenerator(region).generate();
+    new TripsVisualizer(region).render("stops_random.png");
 });
