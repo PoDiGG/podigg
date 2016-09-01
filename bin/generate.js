ea799f95faa0ef905b820281e064807fa9d3320b
Ruben Taelman <ruben.taelman@ugent.be>
2016-09-01 16:18:26 +0200

    Load golden standard stops

--- bin/generate.js
+++ bin/generate.js
@@ -6,12 +6,23 @@ const TripsVisualizer = require('../lib/TripsVisualizer.js');
 const ParameterizedStopsGenerator = require('../lib/ParameterizedStopsGenerator.js');
 const RandomStopsGenerator = require('../lib/RandomStopsGenerator.js');
 
+// Generate stops based on population distribution
 new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
     new ParameterizedStopsGenerator(region).generate();
     new TripsVisualizer(region).render("stops_parameterized.png");
 });
 
+// Generate stops at random
 new RegionFactory('input_data/region_cells.csv').createRegion((region) => {
     new RandomStopsGenerator(region).generate();
     new TripsVisualizer(region).render("stops_random.png");
 });
+
+// Load golden standard of stops
+new RegionFactory('input_data/region_cells.csv', true).createRegion((region) => {
+    new TripsVisualizer(region).render("stops_gs.png");
+});
+
+// TODO:
+// Add function to region to get stop list
+// Compare the two stop lists with the golden standard (calculate distance)
