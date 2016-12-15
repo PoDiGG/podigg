732202106e21d33e28e65ba021a3c3a92d5ee6a2
Ruben Taelman <ruben.taelman@ugent.be>
2016-12-15 13:50:41 +0100

    Automatically create output directory if it does not exist

--- bin/generate-config.js
+++ bin/generate-config.js
@@ -6,5 +6,8 @@ const fs = require('fs');
 
 /* Invoke the generator based on a config file. */
 var path = process.argv.length < 3 ? 'output_data' : process.argv[2];
+if (!fs.existsSync(path)) {
+  fs.mkdirSync(path);
+}
 var config = process.argv.length < 4 ? {} : JSON.parse(fs.readFileSync(process.argv[3]));
 new GtfsGenerator(config).generate(path);
--- bin/generate-env.js
+++ bin/generate-env.js
@@ -2,7 +2,11 @@
 'use strict';
 
 const GtfsGenerator = require('../lib/GtfsGenerator');
+const fs = require('fs');
 
 /* Invoke the generator based on environment variables. */
 var path = process.argv.length < 3 ? 'output_data' : process.argv[2];
+if (!fs.existsSync(path)) {
+  fs.mkdirSync(path);
+}
 GtfsGenerator.fromEnvironmentVariables().generate(path);
